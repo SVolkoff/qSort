83fd7c067f33ae0e6e11363bc4eb3ad4bc1e46da
SVolkoff <sabina-volkoff@yandex.com>
2017-04-30 18:29:18 +0300

    Add files via upload

---
+++ include/qSort.hpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+template <typename Iterator>
+void q_Sort(Iterator first, Iterator last)
+{
+	Iterator f = first, l = last;
+	Iterator x = (first+(last-first)/2);
+	while (f <= l)
+	{
+		while (*f < *x) f++;
+		while (*l > *x) l--;
+		if (f <= l)
+		{
+			std::iter_swap(l, f);
+			f++;
+			l--;
+		}
+	}
+	if (f < last)
+		q_Sort( f, last);
+	if (first< l)
+		q_Sort( first, l);
+}
---
+++ tests/init.cpp
@@ -0,0 +1,35 @@
+#include <qSort.hpp>
+#include <catch.hpp>
+
+
+SCENARIO("mas")
+{
+	int m[] = { 7,2,4,3,5 };
+	int* f=m;
+	int* l=m+5;
+	q_Sort(f, l);
+	REQUIRE(m[0] == 2);
+	REQUIRE(m[1] == 3);
+	REQUIRE(m[2] == 4);
+	REQUIRE(m[3] == 5);
+	REQUIRE(m[4] == 7);
+	
+}
+SCENARIO("matr")
+{
+ 	int mas[3][3] = { 9,6,3,4,82,16,7,91,8 };
+	int * fir = &mas[0][0];
+	int * las = &mas[2][3];
+	q_Sort(fir, las);
+	REQUIRE(mas[0][0] == 3);
+	REQUIRE(mas[0][1] == 4);
+	REQUIRE(mas[0][2] == 6);
+	REQUIRE(mas[1][0] == 7);
+	REQUIRE(mas[1][1] == 8);
+	REQUIRE(mas[1][2] == 9);
+	REQUIRE(mas[2][0] == 16);
+	REQUIRE(mas[2][1] == 82);
+	REQUIRE(mas[2][2] == 91);
+
+	
+}
